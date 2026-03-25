de7120ddc77d598cffd87d744cdd41559f95d5b8
cemalturkcan <cemalturkcann@gmail.com>
2026-03-25 14:51:59 +0300

    fix: Client component renders children in browser mode

--- packages/gea/src/lib/client.ts
+++ packages/gea/src/lib/client.ts
@@ -2,6 +2,9 @@ import Component from './base/component'
 
 export class Client extends Component {
   template() {
-    return `<div id="${this.id}"></div>`
+    if (Component._ssgMode) {
+      return `<div id="${this.id}"></div>`
+    }
+    return `<div id="${this.id}">${this.props?.children || ''}</div>`
   }
 }
